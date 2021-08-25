2772ed19a036b200075a885dc2678d9ecd1eec6c
Puppy2Git <alexander.garcia2722@gmail.com>
2021-08-25 14:52:32 -0500

    Added temp aiming
    It has a sprite to represent direction

---
+++ Assets/Scripts/Player Scripts/rotateScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateScript : MonoBehaviour
+{
+    public GameObject player;
+    public float offset;
+    public float bulletSpeed = 10;
+    public Rigidbody2D bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousepos = Input.mousePosition;
+        mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+
+        Vector2 direction = new Vector2(mousepos.x - transform.position.x, mousepos.y - transform.position.y);
+        transform.position = player.transform.position + new Vector3(0, offset);
+        transform.up = direction;
+        if (Input.GetButtonDown("Fire1"))
+            Fire();
+    }
+
+
+
+    void Fire()
+    {
+        Rigidbody2D bulletClone = (Rigidbody2D)Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = transform.up * bulletSpeed;
+        Debug.Log(transform.up);
+    }
+}
+
