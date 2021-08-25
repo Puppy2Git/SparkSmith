d138d87b6d8f0c9da3c6b7d752e52eb38fbd01c8
Puppy2Git <alexander.garcia2722@gmail.com>
2021-08-25 13:33:20 -0500

    History
    test

--- Assets/Scripts/Player Scripts/Hitboxes/SideBox.cs
+++ Assets/Scripts/Player Scripts/Hitboxes/SideBox.cs
@@ -10,7 +10,7 @@ public class SideBox : MonoBehaviour
     {
         //Gets player script object
         move = transform.parent.parent.GetComponent<CharacterMovement>();
-
+        //Test
         //Ignores Character hitbox and feet should be moved to hitboxes for better organization
         Physics2D.IgnoreCollision(transform.parent.parent.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         Physics2D.IgnoreCollision(transform.parent.Find("Leg Hitbox").GetComponent<Collider2D>(), GetComponent<Collider2D>());
